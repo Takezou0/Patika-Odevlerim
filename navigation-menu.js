01b4bc22b7b8e01dd0e285b463e12fcc5329b968
Takezou0 <bkaya1228@gmail.com>
2023-01-09 05:39:43 +0300

    patika.dev CSS Kursu, Ödev - 1

---
+++ navigation-menu.js
@@ -0,0 +1,8 @@
+document.write(`
+<div class="menu">
+<a class="hoverable-card" href="index.html"> Anasayfa </a>
+<a class="hoverable-card" href="fav-games.html"> Favori Oyunlarım </a>
+<a class="hoverable-card" href="guitar-tabs.html"> Gitar Tableri </a>
+<a class="hoverable-card" href="contact.html"> İletişim </a>
+<a class="hoverable-card" href="recipe.html"> Yemek Tarifi </a>
+</div>`);
